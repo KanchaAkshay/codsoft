50fea090574be69fac67e382cc9c9e8bf5d7ff89
KanchaAkshay <119435591+KanchaAkshay@users.noreply.github.com>
2023-10-24 08:33:43 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,38 +1,26 @@
-#addition
+#addition:
 def add(x, y):
     return x + y
-#substraction
+#substraction:
 def subtract(x, y):
     return x - y
-#multiplication
+#multiplication:
 def multiply(x, y):
     return x * y
-#division
+#division:
 def divide(x, y):
     return x / y
-# modulus
-def mod(x,y):
-    return x % y
-# exponent
-def exp(x,y):
-    return x**y
-# floor division
-def floor(x,y):
-    return x//y
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
-print("5.modulus")
-print("6.exponent")
-print("7.floor division")
 
 while True:
-    choice = input("Enter choice (1/2/3/4/5/6/7): ")
+    choice = input("Enter choice (1/2/3/4): ")
 
-    if choice in ('1','2','3','4','5','6','7'):
+    if choice in ('1','2','3','4'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -52,14 +40,6 @@ while True:
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1,num2))
 
-        elif choice == '5':
-            print(num1, "%", num2, "=", mod(num1,num2))
-
-        elif choice =='6':
-            print(num1, "**",num2, "=", exp(num1,num2))
-
-        elif choice =='7':
-            print(num1, "//",num2, "=", floor(num1,num2))
         next_calculation = input("another calculation? yes/no")
         if next_calculation == "no":
             break
